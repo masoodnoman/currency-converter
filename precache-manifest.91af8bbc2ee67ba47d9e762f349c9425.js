fa85218bd3197894e4f6f6c327814461a19dcd36
Noman Masood <nomanmasood1234@gmail.com>
2020-08-15 19:54:09 -0400

    Updates

---
+++ precache-manifest.91af8bbc2ee67ba47d9e762f349c9425.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3ce678f29c38f38624c197329f6fcaa",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "44cb44280900e647fce8",
+    "url": "/currency-converter/static/css/main.697ec6f8.chunk.css"
+  },
+  {
+    "revision": "7365d83fff62cb0af6f5",
+    "url": "/currency-converter/static/js/2.edb56e9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency-converter/static/js/2.edb56e9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44cb44280900e647fce8",
+    "url": "/currency-converter/static/js/main.95a26e97.chunk.js"
+  },
+  {
+    "revision": "a0730edf8381b13d6dc3",
+    "url": "/currency-converter/static/js/runtime-main.46121f8e.js"
+  }
+]);
